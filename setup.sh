b32993342046520ca3d96c8df08ab24afd291176
Little-Whit <1952218168x@gmail.com>
2025-01-24 22:58:17 +0800

    Add New Config Files

--- setup.sh
+++ setup.sh
@@ -5,9 +5,11 @@ DOTFILES_DIR=~/.dotfiles
 rm ~/.bashrc
 rm ~/.vimrc
 rm ~/.gitconfig
+rm ~/.npmrc
 
 ln -sf $DOTFILES_DIR/.bashrc ~/.bashrc
 ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
+ln -sf $DOTFILES_DIR/.npmrc ~/.npmrc
 ln -sf $DOTFILES_DIR/.gitconfig ~/.gitconfig
 
 echo "Dotfiles setup complete!"
