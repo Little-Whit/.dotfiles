a77a7230ea77c99bb7df74e63acd9587bd21f7d4
Little-Whit <1952218168x@gmail.com>
2025-01-24 23:07:49 +0800

    Fix setup.sh, update.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,32 @@ DOTFILES_DIR=~/.dotfiles
 rm ~/.bashrc
 rm ~/.vimrc
 rm ~/.gitconfig
+rm ~/.profile
 rm ~/.npmrc
 
 ln -sf $DOTFILES_DIR/.bashrc ~/.bashrc
 ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
-ln -sf $DOTFILES_DIR/.npmrc ~/.npmrc
 ln -sf $DOTFILES_DIR/.gitconfig ~/.gitconfig
+ln -sf $DOTFILES_DIR/.profile ~/.profile
+ln -sf $DOTFILES_DIR/.npmrc ~/.npmrc
+
+echo "Symbolic Link Created!"
+
+# Install Necessay Packages
+sudo dpkg --set-selections < installed_packages.txt
+sudo apt-get update
+sudo apt-get dselect-upgrade
+
+echo "Packages Installed!"
+
+# Global Config
+sudo rm -rf /etc
+mkdir /etc
+sudo tar -zxvf etc_configs_backup.tar.gz /etc
+
+# User Config
+rm -rf ~/.config
+mkdir ~/.config
+tar -zxvf user_configs_backup.tar.gz ~/.config
 
 echo "Dotfiles setup complete!"
--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ dpkg --get-selections > installed_packages.txt
 # Global Config
 sudo tar -czvf etc_configs_backup.tar.gz /etc
 # User Config
-tar -czvf user_configs_backup.tar.gz ~/.profile ~/.config
+tar -czvf user_configs_backup.tar.gz ~/.config
